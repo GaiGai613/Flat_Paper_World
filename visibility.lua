02b94d498f0a8265a183463fa13fc6f010e75954
GaiGai <henhaodegaigai@gmail.com>
2017-05-22 19:59:19 -0400

    Add files via upload

--- visibility.lua
+++ visibility.lua
@@ -7,22 +7,38 @@ end
 function visibility:update(x1,y1,x2,y2)
     -- if true then return end
     local mv = game.max_visibility
-    for i = 1 , 1 do
-        for x = x1 , x2 do
-            for y = y1 , y2 do
-                -- If block not exist.
-                local b = game:check_if_block_exist(vec2(x,y))
-                if not b or b.id == 0 then
-                    goto vi_up_next
-                end
-
-                --Get visiblity of blocks near by.
-                local mbv = visibility_round_blocks(vec2(x,y))
-
-                world.blocks[y][x].visibility = mbv
-                
-                ::vi_up_next::
+    for x = x1 , x2 do
+        for y = y1 , y2 do
+            -- If block not exist.
+            local b = game:check_if_block_exist(vec2(x,y))
+            if not b or b.id == 0 then
+                goto vi_up_next
             end
+
+            --Get visiblity of blocks near by.
+            local mbv = visibility_round_blocks(vec2(x,y))
+
+            world.blocks[y][x].visibility = mbv
+            
+            ::vi_up_next::
+        end
+    end
+
+    for x = x1 , x2 do
+        for y = y1 , y2 do
+            local x,y = x2-(x-x1),y2-(y-y1)
+            -- If block not exist.
+            local b = game:check_if_block_exist(vec2(x,y))
+            if not b or b.id == 0 then
+                goto vi_up_next
+            end
+
+            --Get visiblity of blocks near by.
+            local mbv = visibility_round_blocks(vec2(x,y))
+
+            world.blocks[y][x].visibility = mbv
+            
+            ::vi_up_next::
         end
     end
 end
@@ -30,17 +46,18 @@ end
 
 function visibility_round_blocks(p)
     local t = {vec2(1,0),vec2(-1,0),vec2(0,1),vec2(0,-1)}
-    local mv = game:max_visibility
-    local mbv = mv
+    local mv = game.max_visibility
+    local mbv = 0
 
     for k , one_vec in pairs(t) do
-        local pos = one_vecp
+        local pos = one_vec+p
         local b = game:check_if_block_exist(pos)
 
-        if not b or b.id == 0 then return mv end
+        if not b or b.id == 0 then return mv-1 end
         
-        mbv = math.max(mbv,b.max_visibility)
+        mbv = math.max(mbv,b.visibility)
     end
 
+    mbv = math.min(0,mbv-1)
     return mbv
 end
